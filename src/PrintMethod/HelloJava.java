bb85eda8e71080fa9fa9754a6c05b9e6733f38eb
ak1473748 <ak1473748@gmail.com>
2026-03-17 23:29:08 +0530

    init commit

---
+++ src/PrintMethod/HelloJava.java
@@ -0,0 +1,7 @@
+package PrintMethod;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, Cool Java!");
+    }
+}
---
+++ src/PrintMethod/Print.java
@@ -0,0 +1,13 @@
+package PrintMethod;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.println("Friday");
+        System.out.print("Saturday");
+        System.out.print("Sunday");
+    }
+}
