5cb9e1c2c7f85bb7096de37e5fd8c731585ace0d
ak1473748 <ak1473748@gmail.com>
2026-04-10 12:15:23 +0530

    init commit

---
+++ src/Method/ParaAndArgu/GreetingApp.java
@@ -0,0 +1,9 @@
+package Method.ParaAndArgu;
+
+public class GreetingApp {
+        // "name" is parameter here
+    static void greetUser(String name) {
+        System.out.println("Hello, " + name + " !Welcome to My Shop.");
+    }
+
+}
---
+++ src/Method/ParaAndArgu/MainApp.java
@@ -0,0 +1,9 @@
+package Method.ParaAndArgu;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // "Rahul" is the argument passed to the greetUser method
+
+        GreetingApp.greetUser("Rahul");
+    }
+}
